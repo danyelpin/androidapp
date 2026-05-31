49a569d78eb3b6106cdbfbe7a6342e363173cc52
Danyel Pinheiro <danyelpinheiro@gmail.com>
2026-05-31 20:52:12 -0300

    Implementa listagem na tela

---
+++ app/src/main/java/com/example/appandroid/ActivityExibeDados.java
@@ -0,0 +1,31 @@
+package com.example.appandroid;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ActivityExibeDados extends AppCompatActivity {
+
+    TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_exibe_dados);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        Intent i = getIntent();
+        Bundle b = i.getExtras();
+        String nome = b.getString("user");
+        ((TextView)findViewById(R.id.textView)).setText(nome);
+    }
+}
--- app/src/main/java/com/example/appandroid/MainActivity.java
+++ app/src/main/java/com/example/appandroid/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.appandroid;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -24,51 +27,32 @@ import com.skydoves.colorpickerview.listeners.ColorEnvelopeListener;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-    Button buttonMudarCor;
-    SimplePaint simplePaint;
+
+    ListView lv;
+    String nomes[] = new String[] {"Joao", "Hudson", "Maria", "Danyel"};
+
+
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
-        setContentView(R.layout.activity_main);
-
-
-
-    }
-    public void mudaCor(ColorEnvelope envelope){
-        simplePaint.mudarCor(envelope.getColor());
-    }
-
-    //criar um enum talvez no simple paint
-    //pra criar uma linha, ou circulo, retangulo
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState){
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        buttonMudarCor = findViewById(R.id.buttonMudaCor);
-        simplePaint = findViewById(R.id.simplePaint);
 
-        buttonMudarCor.setOnClickListener(v->{
-            new ColorPickerDialog.Builder(this)
-                    .setTitle("Mude a cor do desenho")
-                    .setPreferenceName("MyColorPickerDialog")
-                    .setPositiveButton("Confirmar",
-                            new ColorEnvelopeListener() {
-                                @Override
-                                public void onColorSelected(ColorEnvelope envelope, boolean fromUser) {
-                                    mudaCor(envelope);
-                                }
-                            })
-                    .setNegativeButton("Cancelar",
-                            new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialogInterface, int i) {
-                                    dialogInterface.dismiss();
-                                }
-                            })
-                    .attachAlphaSlideBar(true) // the default value is true.
-                    .attachBrightnessSlideBar(true)  // the default value is true.
-                    .setBottomSpace(12) // set a bottom space between the last slidebar and buttons.
-                    .show();
+        lv = findViewById(R.id.listView);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getApplicationContext(),
+                android.R.layout.simple_list_item_1,
+                android.R.id.text1,
+                nomes
+
+        );
+        lv.setAdapter(adapter);
+
+        lv.setOnItemClickListener((parent, view, position, id)->{
+            Toast.makeText(this, nomes[position], Toast.LENGTH_LONG).show();
+            //
+            Intent i = new Intent(getApplicationContext(),ActivityExibeDados.class);
+            i.putExtra("user", nomes[position]);
+            startActivity(i);
+
         });
     }
 }
