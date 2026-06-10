5592768558c02fb44482890bf41f54f2b581d214
Danyel Pinheiro <danyelpinheiro@gmail.com>
2026-06-10 19:37:15 -0300

    sharedpreferences

--- app/src/main/java/com/example/appandroid/MainActivity.java
+++ app/src/main/java/com/example/appandroid/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.appandroid;
 
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.util.Log;
@@ -25,65 +26,26 @@ import com.skydoves.colorpickerview.listeners.ColorEnvelopeListener;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-    Button buttonMudarCor;
-    SimplePaint simplePaint;
-    ImageView btnCirculo;
-    //        btnRetangulo;
-    //        btnLinha
+    SharedPreferences  sharedPreferences;
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
-        setContentView(R.layout.activity_main);
-
-
-
-    }
-    public void mudaCor(ColorEnvelope envelope){
-        simplePaint.mudarCor(envelope.getColor());
-    }
-
-    //criar um enum talvez no simple paint
-    //pra criar uma linha, ou circulo, retangulo
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState){
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        buttonMudarCor = findViewById(R.id.buttonMudaCor);
-        simplePaint = findViewById(R.id.simplePaint);
-        btnCirculo = findViewById(R.id.tapButton);
-        //tenho que criar um botao para a linha...
-        //btnRetangulo = findViewById(R.id._____).....
-
-        btnCirculo.setOnClickListener(v->{
-            simplePaint.setFerramenta(SimplePaint.Ferramenta.CIRCULO);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main),
+                (v, insets) -> {
+        Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+        v.setPadding(systemBars.left,  systemBars.top,
+                systemBars.right, systemBars.bottom);
+        return insets;
         });
+        sharedPreferences = getSharedPreferences("preferencias", MODE_PRIVATE);
+        sharedPreferences.edit().putString("nome", "joao").apply();
+                //criar uma textview para visualizar
 
-        //btnRetangulo.setOnClickListener(v->{
-        //simplePaint.setFerramente(SimplePaint.Ferramente.RETANGULO);
-        //});
 
-        buttonMudarCor.setOnClickListener(v->{
-            new ColorPickerDialog.Builder(this)
-                    .setTitle("Mude a cor do desenho")
-                    .setPreferenceName("MyColorPickerDialog")
-                    .setPositiveButton("Confirmar",
-                            new ColorEnvelopeListener() {
-                                @Override
-                                public void onColorSelected(ColorEnvelope envelope, boolean fromUser) {
-                                    mudaCor(envelope);
-                                }
-                            })
-                    .setNegativeButton("Cancelar",
-                            new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialogInterface, int i) {
-                                    dialogInterface.dismiss();
-                                }
-                            })
-                    .attachAlphaSlideBar(true) // the default value is true.
-                    .attachBrightnessSlideBar(true)  // the default value is true.
-                    .setBottomSpace(12) // set a bottom space between the last slidebar and buttons.
-                    .show();
-        });
+        //textView.setText(sharedPreferences.getString("nome","").toString());
+
+
     }
 }
