552b3fd54188b5869d0fcb211bde9a0db0524167
Danyel Pinheiro <danyelpinheiro@gmail.com>
2026-06-16 21:59:42 -0300

    sensores e mapa

--- app/src/main/java/com/example/appandroid/MainActivity.java
+++ app/src/main/java/com/example/appandroid/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.appandroid;
 
+import android.Manifest;
 import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.util.Log;
@@ -12,78 +16,104 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.skydoves.colorpickerview.ColorEnvelope;
-import com.skydoves.colorpickerview.ColorPickerDialog;
-import com.skydoves.colorpickerview.listeners.ColorEnvelopeListener;
+//import com.skydoves.colorpickerview.ColorEnvelope;
+//import com.skydoves.colorpickerview.ColorPickerDialog;
+//import com.skydoves.colorpickerview.listeners.ColorEnvelopeListener;
+
+import org.osmdroid.config.Configuration;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.Marker;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-    Button buttonMudarCor;
-    SimplePaint simplePaint;
-    ImageView btnCirculo;
-    //        btnRetangulo;
-    //        btnLinha
+
+    LocationManager locationManager;
+    MapView mapView;
+
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
-        setContentView(R.layout.activity_main);
-
-
-
-    }
-    public void mudaCor(ColorEnvelope envelope){
-        simplePaint.mudarCor(envelope.getColor());
-    }
-
-    //criar um enum talvez no simple paint
-    //pra criar uma linha, ou circulo, retangulo
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        buttonMudarCor = findViewById(R.id.buttonMudaCor);
-        simplePaint = findViewById(R.id.simplePaint);
-        btnCirculo = findViewById(R.id.tapButton);
-        //tenho que criar um botao para a linha...
-        //btnRetangulo = findViewById(R.id._____).....
-
-        btnCirculo.setOnClickListener(v->{
-            simplePaint.setFerramenta(SimplePaint.Ferramenta.CIRCULO);
+        TextView textView = findViewById(R.id.textView);
+        mapView=findViewById(R.id.map);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
         });
+        Configuration.getInstance().setUserAgentValue(getPackageName());
 
-        //btnRetangulo.setOnClickListener(v->{
-        //simplePaint.setFerramente(SimplePaint.Ferramente.RETANGULO);
-        //});
+        mapView.setTileSource(TileSourceFactory.MAPNIK);
+        mapView.setMultiTouchControls(true);
+        checkLocationPermission();
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        //Location localizacao = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,
+                location -> {
 
-        buttonMudarCor.setOnClickListener(v->{
-            new ColorPickerDialog.Builder(this)
-                    .setTitle("Mude a cor do desenho")
-                    .setPreferenceName("MyColorPickerDialog")
-                    .setPositiveButton("Confirmar",
-                            new ColorEnvelopeListener() {
-                                @Override
-                                public void onColorSelected(ColorEnvelope envelope, boolean fromUser) {
-                                    mudaCor(envelope);
-                                }
-                            })
-                    .setNegativeButton("Cancelar",
-                            new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialogInterface, int i) {
-                                    dialogInterface.dismiss();
-                                }
-                            })
-                    .attachAlphaSlideBar(true) // the default value is true.
-                    .attachBrightnessSlideBar(true)  // the default value is true.
-                    .setBottomSpace(12) // set a bottom space between the last slidebar and buttons.
-                    .show();
-        });
+        textView.setText("Latitude" + location.getLatitude() + "\n Longitude" + location.getLatitude());
+        mapView.setMultiTouchControls(true);
+        mapView.getController().setZoom(15.0);
+        double latitude = location.getLatitude();
+        double longitude = location.getLongitude();
+        GeoPoint geoPoint = new GeoPoint(latitude, longitude);
+        mapView.getController().setCenter(geoPoint);
+        mapView.getController().setZoom(18.0);
+        mapView.getController().animateTo(geoPoint);
+        Marker marker = new Marker(mapView);
+        marker.setPosition(geoPoint);
+        marker.setTitle("Voce esta aqui");
+        mapView.getOverlays().add(marker);
+    });
+
+
+
+        /*if (localizacao != null) {
+            double latitude = localizacao.getLatitude();
+            double longitude = localizacao.getLongitude();
+            textView.setText("Latitude" + latitude + "\n Longitude" + longitude);
+        } else {
+            textView.setText("Localizacao nao disponivel");
+        }*/
+
+    }
+
+
+    private static final int REQUEST_LOCATION_PERMISSION = 1;
+
+    private void checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    1);
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[]
+            grantResults, int deviceId) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == 1) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                //Permissao concedida, continuar com operacao
+            } else {
+                //Permissao negada, tratar o caso
+            }
+        }
     }
 }
